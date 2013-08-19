43d73c37b547c84f46233e6cd0cd98af5c70c048
gregpaton08 <gregpaton08@gmail.com>
2013-08-19 18:13:58 -0400

    [add] First commit.

--- fanController/ViewController.m
+++ fanController/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self initNetworkCommunication];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +27,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Network 
+
+- (void)initNetworkCommunication {
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"192.168.0.31", 12345, &readStream, &writeStream);
+    inputStream = (__bridge NSInputStream *)readStream;
+    outputStream = (__bridge NSOutputStream *)writeStream;
+    [inputStream setDelegate:self];
+    [outputStream setDelegate:self];
+    [inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [inputStream open];
+    [outputStream open];
+}
+
+#pragma mark - IBAction
+- (IBAction)turnOnOff:(id)sender {
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"Turn On"]) {
+        NSString *response  = [NSString stringWithFormat:@"1"];
+        NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+        [outputStream write:[data bytes] maxLength:[data length]];
+        [sender setTitle:@"Turn Off" forState:UIControlStateNormal];
+    }
+    else {
+        NSString *response  = [NSString stringWithFormat:@"2"];
+        NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+        [outputStream write:[data bytes] maxLength:[data length]];
+        [sender setTitle:@"Turn On" forState:UIControlStateNormal];
+    }
+}
+
 
 @end
