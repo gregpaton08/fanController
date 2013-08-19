544d3d02139f2ba6fc76ec6164fe9f9ed0b8a540
gregpaton08 <gregpaton08@gmail.com>
2013-08-19 17:53:14 -0400

    Initial Commit

---
+++ fanController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fanController
+//
+//  Created by Greg Paton on 8/19/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fanController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fanController
+//
+//  Created by Greg Paton on 8/19/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
